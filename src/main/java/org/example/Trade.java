02b9521aa95e9fecff10ebce31a5969a386edae7
amnuhu <abdul-majid.nuhu@turntabl.io>
2023-03-30 07:22:18 +0000

    lab1 added time and date instance variables

--- src/main/java/org/example/Trade.java
+++ src/main/java/org/example/Trade.java
@@ -1,5 +1,9 @@
 package org.example;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.Date;
 import java.util.Objects;
 
 public abstract class Trade {
@@ -8,6 +12,10 @@ public abstract class Trade {
     private String symbol;
     private int quantity;
     private double price;
+    private LocalTime tradeTime;
+    private LocalDate tradeDate;
+
+
 
     public Trade(String id, String symbol, int quantity) {
         this(id,symbol, quantity,0.0);
